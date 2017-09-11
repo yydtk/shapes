d3273da1dd09643f2baaea7665cadf54d8465133
yydtk <243195612@qq.com>
2017-09-11 08:48:17 +0800

    “ârevise

--- Shapes/src/shapes/MyPic.java
+++ Shapes/src/shapes/MyPic.java
@@ -4,18 +4,20 @@ public class MyPic {
 	public static void main(String[] args) 
 	{
 		Picture pic = new Picture(420,300);
-		Circle c1 = new Circle(320,40,80);
+		Circle c1 = new Circle(320,40,30);
 		Rectangle r1 = new Rectangle(100, 100, 100, 100);
 		Triangle t1 = new Triangle(100, 100, 200, 100, 150, 50);
-		Line l1 = new Line(0,205,400,205);
+		Line l1 = new Line(0,200,400,200);
 		Circle c2 = new Circle(200,200,50);
 		Circle c3=new Circle(89,25,23);
+		Circle c4=new Circle(300,150,16);
 		pic.add(c1);
 		pic.add(r1);
 		pic.add(t1);
 		pic.add(l1);
 		pic.add(c2);
-//		pic.add(c3);
+		pic.add(c3);
+		pic.add(c4);
 		pic.draw();	
 	}
 }
