b0e9a494051d2b44105a70f5b330bb13d73b7826
yydtk <243195612@qq.com>
2017-09-11 08:35:03 +0800

    add shapes

---
+++ Shapes/src/shapes/MyPic.java
@@ -0,0 +1,21 @@
+package shapes;
+
+public class MyPic {
+	public static void main(String[] args) 
+	{
+		Picture pic = new Picture(420,300);
+		Circle c1 = new Circle(320,40,80);
+		Rectangle r1 = new Rectangle(100, 100, 100, 100);
+		Triangle t1 = new Triangle(100, 100, 200, 100, 150, 50);
+		Line l1 = new Line(0,205,400,205);
+		Circle c2 = new Circle(200,200,50);
+		Circle c3=new Circle(89,25,23);
+		pic.add(c1);
+		pic.add(r1);
+		pic.add(t1);
+		pic.add(l1);
+		pic.add(c2);
+//		pic.add(c3);
+		pic.draw();	
+	}
+}
